25b9e35aed95866526a3de7cd967d5b78c822579
Thomas Ashelford <thomas@ether.com.au>
2010-11-30 23:07:55 +1100

    from_GET should return dates, not datetimes

--- eventtools/models/occurrence.py
+++ eventtools/models/occurrence.py
@@ -321,7 +321,7 @@ class OccurrenceQuerySetFN(object):
 
         if fr is None:
             if to is None:
-                return self.forthcoming(), (datetime.now(), to)
+                return self.forthcoming(), (date.today(), to)
             else:
                 return self.before(to).reverse(), (fr, to) #bleh, results unlikely to be authentic. First person to use this fix it up.
         else:
